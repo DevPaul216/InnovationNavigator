f0d8b35545c864fc361b99b3ac5fbc10a21ca9cd
DevPaul216 <paul.hannibal216@gmail.com>
2025-06-03 13:11:50 +0200

    Update streamlit_modular.py

--- src/streamlit_modular.py
+++ src/streamlit_modular.py
@@ -763,7 +763,7 @@ def chart_view():
             <div style="
                 position: fixed;
                 top: 220px;
-                left: 25%;
+                left: 45%;
                 transform: translateX(-50%);
                 z-index: 9999;
                 pointer-events: none;
