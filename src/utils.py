e0fccf472fb785debd0fbefe37636c1e82fcca39
DevPaul216 <paul.hannibal216@gmail.com>
2025-06-04 00:07:18 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -108,7 +108,7 @@ def make_request_image(prompt, model="gpt-image-1", additional_information_dict=
         model=model,
         prompt=prompt,
         size="1024x1024",
-        quality="standard",
+        quality="low",
         n=1
     )
 
