c3023b691b13c8f2e04b58e1da64542b85d97fbf
DevPaul216 <paul.hannibal216@gmail.com>
2025-06-03 21:58:20 +0200

    asg

--- src/streamlit_modular.py
+++ src/streamlit_modular.py
@@ -27,7 +27,6 @@ data_store_path = os.path.join("stores", "data_stores")
 COLOR_BLOCKED = "rgb(250, 240, 220)"
 COLOR_COMPLETED = "rgb(104, 223, 200)"
 COLOR_IN_PROGRESS = "rgb(255, 165, 0)"
-COLOR_PARTLY_COMPLETED = "rgb(180, 200, 255)"  # New color for partly completed
 
 
 def align_data_store():
@@ -187,7 +186,7 @@ def get_config_value(name, for_template=True, config_value="display_name", defau
     return display_name
 
 
-def init_flow_graph(connection_states, completed_templates, blocked_templates, partly_completed_templates=None):
+def init_flow_graph(connection_states, completed_templates, blocked_templates):
     if sst.update_graph:
         nodes = []
         for i, template_name in enumerate(sst.template_config.keys()):
@@ -203,8 +202,6 @@ def init_flow_graph(connection_states, completed_templates, blocked_templates, p
             else:
                 if template_name in blocked_templates:
                     style = {'background-color': COLOR_BLOCKED, "color": 'black'}
-                elif partly_completed_templates and template_name in partly_completed_templates:
-                    style = {'background-color': COLOR_PARTLY_COMPLETED, "color": 'black'}
                 elif template_name in completed_templates:
                     style = {'background-color': COLOR_COMPLETED, "color": 'black'}
                 else:
@@ -231,32 +228,20 @@ def init_flow_graph(connection_states, completed_templates, blocked_templates, p
 def init_graph():
     connection_states = {}
     completed_templates = []
-    partly_completed_templates = []  # New list for partly completed
     for template_name, template_config in sst.template_config.items():
         is_fulfilled = True
-        is_partly_fulfilled = False
         is_required = "required" not in template_config or template_config["required"]
         if is_required:
             elements = template_config["elements"]
-            required_elements = [e for e in elements if sst.elements_config[e]["required"]]
-            num_required = len(required_elements)
-            num_filled = 0
-            for element_name in required_elements:
-                element_store = sst.data_store[template_name]
-                element_values = element_store[element_name]
-                if element_values is not None and len(element_values) > 0:
-                    num_filled += 1
-            if num_filled == num_required:
-                is_fulfilled = True
-            elif num_filled > 0:
-                is_fulfilled = False
-                is_partly_fulfilled = True
-            else:
-                is_fulfilled = False
+            for element_name in elements:
+                element_config = sst.elements_config[element_name]
+                if element_config["required"]:
+                    element_store = sst.data_store[template_name]
+                    for element_values in element_store.values():
+                        if element_values is None or len(element_values) == 0:
+                            is_fulfilled = False
         if is_fulfilled:
             completed_templates.append(template_name)
-        elif is_partly_fulfilled:
-            partly_completed_templates.append(template_name)
         for target in template_config["connects"]:
             edge_id = f"{template_name}-{target}"
             connection_states[edge_id] = is_fulfilled
@@ -266,7 +251,7 @@ def init_graph():
         if template_name not in completed_templates or template_name in blocked_templates:
             blocked_templates.extend(connections)
     blocked_templates = list(set(blocked_templates))
-    return connection_states, completed_templates, blocked_templates, partly_completed_templates
+    return connection_states, completed_templates, blocked_templates
 
 
 def add_artifact(toggle_key, element_name, artifact_id, artifact):
@@ -769,11 +754,6 @@ def legend_subview():
     with legend_cols[2]:
         st.markdown(
             f"<div style='background-color: {COLOR_IN_PROGRESS}; width: 20px; height: 20px; display: inline-block;'></div> Next Step",
-            unsafe_allow_html=True,
-        )
-    with legend_cols[3]:
-        st.markdown(
-            f"<div style='background-color: {COLOR_PARTLY_COMPLETED}; width: 20px; height: 20px; display: inline-block;'></div> Partly Completed",
             unsafe_allow_html=True,
         )
 
