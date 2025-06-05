ae1e56255b6a2b5fd9c11bef7f7249ad22caf519
DevPaul216 <paul.hannibal216@gmail.com>
2025-06-05 02:13:03 +0200

    Update streamlit_modular.py

--- src/streamlit_modular.py
+++ src/streamlit_modular.py
@@ -940,7 +940,7 @@ def general_creation_view(assigned_elements):
                 st.markdown("**Assigned & Available Artifacts**")
             display_generated_artifacts_view(element_name)
             if not manual:
-                st.divider()
+                #st.divider()
 
     if creation_mode == "Manual":
         if is_single:
